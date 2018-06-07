6d3e88e182f66ea1770ab88eaed90a0c023d61bd
Mac <mac@MacBook-Pro.local>
2018-06-07 16:21:19 -0600

    implemented exact route for home route and added withRouter HOC into the searchbar to navigate the user to results, and fixed some bugs

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { Field , reduxForm } from 'redux-form';
+
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
+
+    handleFormSubmit = function({query}) {
+        console.log('trying to handle query', query);
+        this.props.history.push('/results');
+    }
+
+    renderInput(field) {
+            return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+        }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
-                <input placeholder="Search DailySmarty" />
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput} />
             </form>
         )
     }
 }
+
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
